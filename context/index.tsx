f8349e6e3d5f78711ffd45f6ddfffa67594da7c3
Evan Godon <e.godon@outlook.com>
2019-09-19 17:22:05 -0400

    Delete unused function

--- context/index.tsx
+++ context/index.tsx
@@ -1,16 +1,4 @@
-import * as React from 'react';
 import { ModalProvider, useModalContext } from './modal-context';
 import { LinksProvider, useLinksContext } from './links-context';
-
-export function createCtx<A>() {
-  const ctx = React.createContext<A | undefined>(undefined);
-
-  function useCtx() {
-    const c = React.useContext(ctx);
-    if (!c) throw new Error('useCtx must be inside a Provider with a value');
-    return c;
-  }
-  return [useCtx, ctx.Provider] as const;
-}
 
 export { ModalProvider, useModalContext, LinksProvider, useLinksContext };
