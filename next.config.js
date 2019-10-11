395c850622eaf10a5ad0b056a52a18ebdc244681
Evan Godon <e.godon@outlook.com>
2019-10-11 17:35:55 -0400

    Configure now secrets properly

--- next.config.js
+++ next.config.js
@@ -4,18 +4,15 @@ const Dotenv = require('dotenv-webpack');
 require('dotenv').config();
 
 module.exports = {
-  webpack: (config) => {
-    config.plugins = config.plugins || [];
-
-    config.plugins = [
-      ...config.plugins,
-
-      new Dotenv({
-        path: path.join(__dirname, '.env'),
-        systemvars: true,
-      }),
-    ];
-
-    return config;
+  env: {
+    firebaseConfig: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: '',
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+    },
   },
 };
--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,15 +1,13 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: '',
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-};
+const config = process.env.firebaseConfig;
+
+if (!config) {
+  throw new Error('Firebase config missing.');
+}
+
+console.log({ config });
 
 const firebaseConfig = !!firebase.apps[0]
   ? firebase.app()
